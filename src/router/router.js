e2a6e76870fe35a1fe01457e8ac7f87ae2c735ef
Charles Langlois <charles.langlois.dev@gmail.com>
2024-11-30 14:16:03 +0100

    Feat/base/app & dependencies

---
+++ src/router/router.js
@@ -0,0 +1,54 @@
+import { createBrowserRouter } from "react-router-dom";
+// import Banner from "../components/Banner.tsx";
+// import Footer from "../components/Footer.tsx";
+
+// import img from "../assets/images/background7.jpg";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <div className="main-container">
+        {/* <Banner /> */}
+        <main>
+          <div className="container-image">
+            {/* <img className="image" src={img}></img> */}
+          </div>
+        </main>
+        {/* <Footer /> */}
+      </div>
+    ),
+  },
+  {
+    path: "/penal-code",
+    element: (
+      <div className="main-container">
+        {/* <Banner /> */}
+        <main></main>
+        {/* <Footer /> */}
+      </div>
+    ),
+  },
+  {
+    path: "/about",
+    element: (
+      <div className="main-container">
+        {/* <Banner /> */}
+        <main></main>
+        {/* <Footer /> */}
+      </div>
+    ),
+  },
+  {
+    path: "*",
+    element: (
+      <div className="main-container">
+        {/* <Banner /> */}
+        <main></main>
+        {/* <Footer /> */}
+      </div>
+    ),
+  },
+]);
+
+export default router;
