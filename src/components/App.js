13e4e4948162931a42f12d1e709d4cc8afcc54b0
Charles Langlois <charles.langlois.dev@gmail.com>
2024-12-01 13:06:02 +0100

    Feat/Loader.tsx & style

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,28 @@
+import React, { useState, useEffect } from "react";
 import { RouterProvider } from "react-router-dom";
 import router from "../router/router";
+import Loader from "./Loader.tsx";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      await new Promise((resolve) => setTimeout(resolve, 3000));
+      setLoading(false);
+    };
+
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return (
+      <div className="loader-container">
+        <Loader />
+      </div>
+    );
+  }
+
   return <RouterProvider router={router} />;
 }
 
