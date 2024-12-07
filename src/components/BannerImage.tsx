24860ce41c21084910154cac551e647fc8fc2d8b
Charles Langlois <charles.langlois.dev@gmail.com>
2024-12-07 14:23:15 +0100

    Fix/links/home/page

--- src/components/BannerImage.tsx
+++ src/components/BannerImage.tsx
@@ -4,7 +4,13 @@ import "../sass/BannerImage.scss"
 
 import image from '../assets/images/organa-rp.webp';
 
+const discord = "https://discord.gg/9nKDu7UJug";
+
 const BannerImage = () => {
+  const handleClick = () => {
+    window.location.href = discord;
+  };
+
     return (
         <section className="container-image">
             <img className="image" src={image} alt="une voiture roulant vers l'horizon"></img>
@@ -12,7 +18,7 @@ const BannerImage = () => {
               <p className="container-button--txt">
                 Tu cherches un endroit où tout est possible ?
               </p>
-              <Button className='button' onClick={() => console.log("Button clicked!")}>
+              <Button className='button' onClick={handleClick}>
                 Rejoins Nous !
               </Button>
             </div>
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,14 +2,20 @@ import React from 'react'
 import { Gamepad2 } from 'lucide-react'
 import '../sass/Footer.scss'
 
+const discord = "https://discord.gg/9nKDu7UJug";
+
 const Footer = () => {
+  const handleClick = () => {
+    window.location.href = discord;
+  };
+
   return (
     <footer className="footer">
       <div className="footer-content">
         <p className="copyright">
           &copy; 2024 Vizir-dev.fr. All rights reserved.
         </p>
-        <Gamepad2 className="gamepad-icon" />
+        <Gamepad2 className="gamepad-icon" onClick={handleClick} />
       </div>
     </footer>
   )
